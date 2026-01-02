18591570b1ef39abeb26316eb1d71194f76104c1
bbang2u <53766785+bbang2u@users.noreply.github.com>
2026-01-02 14:41:20 +0900

    Create 올바른 괄호.cpp

---
+++ programmers/올바른 괄호.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int count = 0;
+    for(int i=0;i<s.length();i++){
+        if(s[i] == '('){
+            count++;
+        }
+        else{
+            if(count == 0){return false;}
+            count--;
+        }
+    }
+    if(count != 0){return false;}
+    return answer;
+}

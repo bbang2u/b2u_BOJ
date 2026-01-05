a3a133b74438b2697a1c3f24f0d9be1420847fac
bbang2u <53766785+bbang2u@users.noreply.github.com>
2026-01-05 18:16:09 +0900

    Create 가장 큰 수.cpp

---
+++ programmers/가장 큰 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int a, int b){
+    string A = to_string(a) + to_string(b);
+    string B = to_string(b) + to_string(a);
+    return A > B;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    sort(numbers.begin(), numbers.end(), compare);
+    for(int i=0;i<numbers.size();i++){
+        answer+=to_string(numbers[i]);
+    }
+    if (answer[0] == '0') return "0";
+    return answer;
+    
+}
